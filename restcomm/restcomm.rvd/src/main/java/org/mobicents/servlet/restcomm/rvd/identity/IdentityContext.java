81c05b0c53d5a93468a560be1d8238cf4eb99c19
Orestis Tsakiridis <orestis.tsakiridis@telestax.com>
2016-04-22 15:32:56 +0300

    RVD now reads Identity-specific configuration from restcomm.xml. Namely, auth-server-url, realm and realm-public-key options are read.
    Refers #1012

---
+++ restcomm/restcomm.rvd/src/main/java/org/mobicents/servlet/restcomm/rvd/identity/IdentityContext.java
@@ -0,0 +1,11 @@
+package org.mobicents.servlet.restcomm.rvd.identity;
+
+/**
+ * @author Orestis Tsakiridis
+ */
+public class IdentityContext {
+    String authServerUrl;
+    String realm;
+    String realmPublicKey;
+
+}
